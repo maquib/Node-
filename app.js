d7fcfd48e037d00084ba664f389d5d9ad4a1eab2
Mohammad Aquib <mohammad.aquib@quytech.com>
2022-07-04 02:20:31 +0530

    nod ejs

---
+++ app.js
@@ -0,0 +1,5 @@
+import * as fs from './utils.js';
+console.log(fs.name);
+
+const sum =  fs.add(6,-4);
+console.log(sum)
---
+++ utils.js
@@ -0,0 +1,8 @@
+console.log('aquib');
+
+const name = 'Osama';
+const add = (a,b)=>{
+    return a + b;
+}
+
+export {name,add};
